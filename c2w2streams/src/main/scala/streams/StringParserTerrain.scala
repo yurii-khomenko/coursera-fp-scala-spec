0312728e0b53bf49c7cbfade6d1832b9c52edd3d
yykhomenko <yykhomenko@gmail.com>
2017-11-23 18:48:53 +0200

    added second function of main week task

--- c2w2streams/src/main/scala/streams/StringParserTerrain.scala
+++ c2w2streams/src/main/scala/streams/StringParserTerrain.scala
@@ -52,11 +52,12 @@ trait StringParserTerrain extends GameDef {
     * a valid position (not a '-' character) inside the terrain described
     * by `levelVector`.
     */
-  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean =
-    pos =>
-      if (pos.row < 0 || pos.row >= levelVector.length ||
-        pos.col < 0 || pos.col >= levelVector(pos.row).length) false
-      else !levelVector(pos.row)(pos.col).equals('-')
+  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = pos =>
+    pos.row >= 0 &&
+      pos.row < levelVector.length &&
+      pos.col >= 0 &&
+      pos.col < levelVector(pos.row).length &&
+      !levelVector(pos.row)(pos.col).equals('-')
 
   /**
     * This function should return the position of character `c` in the
@@ -66,7 +67,13 @@ trait StringParserTerrain extends GameDef {
     * Hint: you can use the functions `indexWhere` and / or `indexOf` of the
     * `Vector` class
     */
-  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = ???
+  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = {
+
+    val row = levelVector.indexWhere(_ contains c)
+    val col = levelVector(row).indexOf(c)
+
+    Pos(row, col)
+  }
 
   private lazy val vector: Vector[Vector[Char]] =
     Vector(level.split("\n").map(str => Vector(str: _*)): _*)
