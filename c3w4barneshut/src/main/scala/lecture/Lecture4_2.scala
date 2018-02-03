0d657a1cf7957dff6f11296813b6893470157dcf
yykhomenko <yykhomenko@gmail.com>
2018-02-03 20:28:22 +0200

    added lecture 4.2

---
+++ c3w4barneshut/src/main/scala/lecture/Lecture4_2.scala
@@ -0,0 +1,100 @@
+package lecture
+
+import common._
+import org.scalameter._
+
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.parallel.{Combiner, ForkJoinTaskSupport}
+import scala.concurrent.forkjoin.ForkJoinPool
+import scala.reflect.ClassTag
+
+class ArrayCombiner[T <: AnyRef : ClassTag](val parallelism: Int)
+  extends Combiner[T, Array[T]] {
+
+  private var numElems = 0
+  private val buffers = new ArrayBuffer[ArrayBuffer[T]]
+  buffers += new ArrayBuffer[T]
+
+  def size = numElems
+
+  def clear() = buffers.clear()
+
+  def +=(elem: T) = {
+    buffers.last += elem
+    numElems += 1
+    this
+  }
+
+  def combine[N <: T, That >: Array[T]](that: Combiner[N, That]) =
+    (that: @unchecked) match {
+      case that: ArrayCombiner[T] =>
+        buffers ++= that.buffers
+        numElems += that.numElems
+        this
+    }
+
+  private def copyTo(array: Array[T], from: Int, end: Int): Unit = {
+
+    var i = from
+    var j = 0
+
+    while (i >= buffers(j).length) {
+      i -= buffers(j).length
+      j += 1
+    }
+
+    var k = from
+    while (k < end) {
+      array(k) = buffers(j)(i)
+      i += 1
+      if (i >= buffers(j).length) {
+        i = 0
+        j += 1
+      }
+      k += 1
+    }
+  }
+
+  def result: Array[T] = {
+    val array = new Array[T](numElems)
+    val step = math.max(1, numElems / parallelism)
+    val starts = (0 until numElems by step) :+ numElems
+    val chunks = starts zip starts.tail
+    val tasks = for ((from, end) <- chunks) yield task {
+      copyTo(array, from, end)
+    }
+    tasks.foreach(_.join())
+    array
+  }
+}
+
+object lecture3_2 extends App {
+
+  val standardConfig = config(
+    Key.exec.minWarmupRuns -> 20,
+    Key.exec.maxWarmupRuns -> 40,
+    Key.exec.benchRuns -> 60,
+    Key.verbose -> true
+  ) withWarmer new Warmer.Default
+
+  val size = 1000000
+
+  def run(p: Int): Unit = {
+
+    val taskSupport = new ForkJoinTaskSupport(new ForkJoinPool(p))
+    val strings = (0 until size) map(_.toString)
+
+    val time = standardConfig measure {
+      val parallelized = strings.par
+      parallelized.tasksupport = taskSupport
+      def newCombiner = new ArrayCombiner(p): Combiner[String, Array[String]]
+      parallelized.aggregate(newCombiner)(_ += _, _ combine _)
+    }
+    println(s"p = $p, time = $time ms")
+  }
+
+  run(1)
+  run(2)
+  run(4)
+  run(8)
+}
