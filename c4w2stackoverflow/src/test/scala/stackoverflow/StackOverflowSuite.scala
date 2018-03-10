fb54159a7b7717620e7d4bcdc5b751ad27a7c397
yykhomenko <yykhomenko@gmail.com>
2018-03-10 21:04:51 +0200

    added groupedPostings test

--- c4w2stackoverflow/src/test/scala/stackoverflow/StackOverflowSuite.scala
+++ c4w2stackoverflow/src/test/scala/stackoverflow/StackOverflowSuite.scala
@@ -1,11 +1,24 @@
 package stackoverflow
 
+import org.apache.spark.{SparkConf, SparkContext}
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{BeforeAndAfterAll, FunSuite}
 
 @RunWith(classOf[JUnitRunner])
 class StackOverflowSuite extends FunSuite with BeforeAndAfterAll {
+
+  val conf = new SparkConf().setMaster("local").setAppName("StackOverflow")
+  val sc = new SparkContext(conf)
+
+  val app = new StackOverflow
+
+  val question = Posting(1, 101, Some(102), None, 9, Some("Scala"))
+  val answer = Posting(2, 102, None, Some(101), 8, None)
+
+  val posts = List(question, answer)
+
+  val raw = sc.parallelize(posts)
 
   lazy val testObject = new StackOverflow {
     override val langs =
@@ -19,6 +32,8 @@ class StackOverflowSuite extends FunSuite with BeforeAndAfterAll {
     override def kmeansMaxIterations = 120
   }
 
+  override def afterAll() = sc.stop()
+
   test("testObject can be instantiated") {
     val instantiatable = try {
       testObject
@@ -27,5 +42,13 @@ class StackOverflowSuite extends FunSuite with BeforeAndAfterAll {
       case _: Throwable => false
     }
     assert(instantiatable, "Can't instantiate a StackOverflow object")
+  }
+
+  test("groupedPostings works correctly") {
+
+    val expected = Array((101, Seq((question, answer))))
+    val grouped = app.groupedPostings(raw).collect()
+
+    assert(grouped === expected)
   }
 }
