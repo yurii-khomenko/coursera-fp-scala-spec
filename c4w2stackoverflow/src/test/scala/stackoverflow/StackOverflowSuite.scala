ac29728b73c4aefae79a0c8fc098becd15af05bf
yykhomenko <yykhomenko@gmail.com>
2018-03-13 08:08:31 +0200

    refactored tests

--- c4w2stackoverflow/src/test/scala/stackoverflow/StackOverflowSuite.scala
+++ c4w2stackoverflow/src/test/scala/stackoverflow/StackOverflowSuite.scala
@@ -10,7 +10,6 @@ class StackOverflowSuite extends FunSuite with BeforeAndAfterAll {
 
   val conf = new SparkConf().setMaster("local").setAppName("StackOverflow")
   val sc = new SparkContext(conf)
- // val app = new StackOverflow
 
   lazy val testObject = new StackOverflow {
     override val langs =
@@ -22,6 +21,16 @@ class StackOverflowSuite extends FunSuite with BeforeAndAfterAll {
     override def kmeansKernels = 45
     override def kmeansEta: Double = 20.0D
     override def kmeansMaxIterations = 120
+
+    val q1 = Posting(1, 101, None, None, 8, Some("Scala"))
+    val q2 = Posting(1, 102, Some(103), None, 7, Some("Java"))
+
+    val a1 = Posting(2, 103, None, Some(102), 3, None)
+    val a2 = Posting(2, 104, None, Some(101), 4, None)
+    val a3 = Posting(2, 105, None, Some(101), 5, None)
+
+    val posts = List(q1, q2, a1, a2, a3)
+    val raw = sc.parallelize(posts)
   }
 
   override def afterAll() = sc.stop()
@@ -37,6 +46,7 @@ class StackOverflowSuite extends FunSuite with BeforeAndAfterAll {
   }
 
   test("groupedPostings works correctly") {
+    import testObject._
 
     val question = Posting(1, 101, None, None, 9, Some("Scala"))
     val answer = Posting(2, 102, None, Some(101), 8, None)
@@ -44,47 +54,28 @@ class StackOverflowSuite extends FunSuite with BeforeAndAfterAll {
     val posts = List(question, answer)
     val raw = sc.parallelize(posts)
 
-    val actual = testObject.groupedPostings(raw).collect()
+    val actual = groupedPostings(raw).collect()
     val expected = Array((question.id, Seq((question, answer))))
-
-    assert(actual === expected)
   }
 
   test("scoredPostings works correctly") {
+    import testObject._
 
-    val q1 = Posting(1, 101, None, None, 8, Some("Scala"))
-    val q2 = Posting(1, 102, Some(103), None, 7, Some("Java"))
+    val grouped = groupedPostings(raw)
 
-    val a1 = Posting(2, 103, None, Some(102), 3, None)
-    val a2 = Posting(2, 104, None, Some(101), 4, None)
-    val a3 = Posting(2, 105, None, Some(101), 5, None)
-
-    val posts = List(q1, q2, a1, a2, a3)
-    val raw = sc.parallelize(posts)
-    val grouped = testObject.groupedPostings(raw)
-
-    val actual = testObject.scoredPostings(grouped).collect()
+    val actual = scoredPostings(grouped).collect()
     val expected = Array((q1, 5), (q2, 3))
 
     assert(actual === expected)
   }
 
   test("vectorPostings works correctly") {
+    import testObject._
 
-    val q1 = Posting(1, 101, None, None, 8, Some("Scala"))
-    val q2 = Posting(1, 102, Some(103), None, 7, Some("Java"))
+    val grouped = groupedPostings(raw)
+    val scored = scoredPostings(grouped)
 
-    val a1 = Posting(2, 103, None, Some(102), 3, None)
-    val a2 = Posting(2, 104, None, Some(101), 4, None)
-    val a3 = Posting(2, 105, None, Some(101), 5, None)
-
-    val posts = List(q1, q2, a1, a2, a3)
-    val raw = sc.parallelize(posts)
-
-    val grouped = testObject.groupedPostings(raw)
-    val scored = testObject.scoredPostings(grouped)
-
-    val actual = testObject.vectorPostings(scored).collect()
+    val actual = vectorPostings(scored).collect()
     val expected = Array((500000, 5), (50000, 3))
 
     assert(actual === expected)
