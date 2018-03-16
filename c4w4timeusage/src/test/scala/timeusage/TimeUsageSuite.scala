2416ed6967e83fd6f0b0140ff39b6901e58d9926
yykhomenko <yykhomenko@gmail.com>
2018-03-16 23:03:22 +0200

    init c4w4

---
+++ c4w4timeusage/src/test/scala/timeusage/TimeUsageSuite.scala
@@ -0,0 +1,10 @@
+package timeusage
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{BeforeAndAfterAll, FunSuite}
+
+@RunWith(classOf[JUnitRunner])
+class TimeUsageSuite extends FunSuite with BeforeAndAfterAll {
+
+}
